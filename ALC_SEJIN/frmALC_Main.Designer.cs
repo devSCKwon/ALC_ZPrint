0e8b432c2ca560fccfd2ef507a2dbcf68ac8ff75
devSCKwon <shry19@gmail.com>
2022-01-28 14:34:45 +0900

    ALC_SEJIN 프로젝트 추가
    - 조립장용 ZPL 프린트 프로그램 전용
    - 향후 POS 프로그램내 전용 프로그램 연동하여 개발 예정

--- ALC_SEJIN/frmALC_Main.Designer.cs
+++ ALC_SEJIN/frmALC_Main.Designer.cs
@@ -35,19 +35,18 @@ namespace ALC_SEJIN
             this.tsmiPROGList = new System.Windows.Forms.ToolStripMenuItem();
             this.tsmiPROGListOption1 = new System.Windows.Forms.ToolStripMenuItem();
             this.tsmiPROGListOption2 = new System.Windows.Forms.ToolStripMenuItem();
-            this.tsmiZPLData = new System.Windows.Forms.ToolStripMenuItem();
-            this.tsmiZPLDataOption1 = new System.Windows.Forms.ToolStripMenuItem();
-            this.tsmiZPLDataOption2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiZPLPrintSize = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiZPLPrintSizeOption1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiZPLPrintSizeOption2 = new System.Windows.Forms.ToolStripMenuItem();
             this.tsmiURL = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.tsmiZPLSave = new System.Windows.Forms.ToolStripMenuItem();
-            this.tsmiZPLExit = new System.Windows.Forms.ToolStripMenuItem();
             this.tsmiExit = new System.Windows.Forms.ToolStripMenuItem();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.btnShowJson = new System.Windows.Forms.Button();
             this.btnPrint = new System.Windows.Forms.Button();
             this.txtTemp = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtPNO = new System.Windows.Forms.TextBox();
             this.menuStrip1.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
@@ -75,11 +74,10 @@ namespace ALC_SEJIN
             // 
             this.tsmiZPLMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.tsmiPROGList,
-            this.tsmiZPLData,
+            this.tsmiZPLPrintSize,
             this.tsmiURL,
             this.toolStripSeparator1,
-            this.tsmiZPLSave,
-            this.tsmiZPLExit});
+            this.tsmiZPLSave});
             this.tsmiZPLMenu.Name = "tsmiZPLMenu";
             this.tsmiZPLMenu.Size = new System.Drawing.Size(58, 20);
             this.tsmiZPLMenu.Text = "설정(&B)";
@@ -91,7 +89,7 @@ namespace ALC_SEJIN
             this.tsmiPROGListOption2});
             this.tsmiPROGList.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.tsmiPROGList.Name = "tsmiPROGList";
-            this.tsmiPROGList.Size = new System.Drawing.Size(180, 22);
+            this.tsmiPROGList.Size = new System.Drawing.Size(162, 22);
             this.tsmiPROGList.Text = "공정선택";
             // 
             // tsmiPROGListOption1
@@ -109,58 +107,54 @@ namespace ALC_SEJIN
             this.tsmiPROGListOption2.Text = "글라스 후가공(A010)";
             this.tsmiPROGListOption2.Click += new System.EventHandler(this.tsmiPROGListOption_Click);
             // 
-            // tsmiZPLData
+            // tsmiZPLPrintSize
             // 
-            this.tsmiZPLData.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.tsmiZPLDataOption1,
-            this.tsmiZPLDataOption2});
-            this.tsmiZPLData.Name = "tsmiZPLData";
-            this.tsmiZPLData.Size = new System.Drawing.Size(180, 22);
-            this.tsmiZPLData.Text = "출력사이즈 선택";
+            this.tsmiZPLPrintSize.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiZPLPrintSizeOption1,
+            this.tsmiZPLPrintSizeOption2});
+            this.tsmiZPLPrintSize.Name = "tsmiZPLPrintSize";
+            this.tsmiZPLPrintSize.Size = new System.Drawing.Size(162, 22);
+            this.tsmiZPLPrintSize.Text = "출력사이즈 선택";
             // 
-            // tsmiZPLDataOption1
+            // tsmiZPLPrintSizeOption1
             // 
-            this.tsmiZPLDataOption1.Name = "tsmiZPLDataOption1";
-            this.tsmiZPLDataOption1.Size = new System.Drawing.Size(180, 22);
-            this.tsmiZPLDataOption1.Text = "ALC(55×25)";
-            this.tsmiZPLDataOption1.Click += new System.EventHandler(this.tsmiZPLDataOption_Click);
+            this.tsmiZPLPrintSizeOption1.Name = "tsmiZPLPrintSizeOption1";
+            this.tsmiZPLPrintSizeOption1.Size = new System.Drawing.Size(140, 22);
+            this.tsmiZPLPrintSizeOption1.Text = "ALC(55×25)";
+            this.tsmiZPLPrintSizeOption1.Click += new System.EventHandler(this.tsmiZPLPrintSizeOption_Click);
             // 
-            // tsmiZPLDataOption2
+            // tsmiZPLPrintSizeOption2
             // 
-            this.tsmiZPLDataOption2.Name = "tsmiZPLDataOption2";
-            this.tsmiZPLDataOption2.Size = new System.Drawing.Size(180, 22);
-            this.tsmiZPLDataOption2.Text = "ALC(50×20)";
-            this.tsmiZPLDataOption2.Click += new System.EventHandler(this.tsmiZPLDataOption_Click);
+            this.tsmiZPLPrintSizeOption2.Name = "tsmiZPLPrintSizeOption2";
+            this.tsmiZPLPrintSizeOption2.Size = new System.Drawing.Size(140, 22);
+            this.tsmiZPLPrintSizeOption2.Text = "ALC(50×20)";
+            this.tsmiZPLPrintSizeOption2.Click += new System.EventHandler(this.tsmiZPLPrintSizeOption_Click);
             // 
             // tsmiURL
             // 
             this.tsmiURL.Name = "tsmiURL";
-            this.tsmiURL.Size = new System.Drawing.Size(180, 22);
+            this.tsmiURL.Size = new System.Drawing.Size(162, 22);
             this.tsmiURL.Text = "Data URL";
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(159, 6);
             // 
             // tsmiZPLSave
             // 
             this.tsmiZPLSave.Name = "tsmiZPLSave";
             this.tsmiZPLSave.ShowShortcutKeys = false;
-            this.tsmiZPLSave.Size = new System.Drawing.Size(180, 22);
+            this.tsmiZPLSave.Size = new System.Drawing.Size(162, 22);
             this.tsmiZPLSave.Text = "설정저장(&S)";
-            // 
-            // tsmiZPLExit
-            // 
-            this.tsmiZPLExit.Name = "tsmiZPLExit";
-            this.tsmiZPLExit.Size = new System.Drawing.Size(180, 22);
-            this.tsmiZPLExit.Text = "설정종료(&X)";
+            this.tsmiZPLSave.Click += new System.EventHandler(this.tsmiZPLSave_Click);
             // 
             // tsmiExit
             // 
             this.tsmiExit.Name = "tsmiExit";
             this.tsmiExit.Size = new System.Drawing.Size(110, 20);
             this.tsmiExit.Text = "프로그램 종료(&X)";
+            this.tsmiExit.Click += new System.EventHandler(this.tsmiExit_Click);
             // 
             // tableLayoutPanel1
             // 
@@ -170,7 +164,7 @@ namespace ALC_SEJIN
             this.tableLayoutPanel1.Controls.Add(this.btnShowJson, 1, 2);
             this.tableLayoutPanel1.Controls.Add(this.btnPrint, 1, 1);
             this.tableLayoutPanel1.Controls.Add(this.txtTemp, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtPNO, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -203,6 +197,7 @@ namespace ALC_SEJIN
             this.btnPrint.TabIndex = 16;
             this.btnPrint.Text = "PRINT";
             this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
             // 
             // txtTemp
             // 
@@ -216,17 +211,17 @@ namespace ALC_SEJIN
             this.txtTemp.TabIndex = 15;
             this.txtTemp.TabStop = false;
             // 
-            // textBox1
+            // txtPNO
             // 
-            this.tableLayoutPanel1.SetColumnSpan(this.textBox1, 2);
-            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBox1.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.textBox1.Location = new System.Drawing.Point(3, 3);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(794, 26);
-            this.textBox1.TabIndex = 0;
+            this.tableLayoutPanel1.SetColumnSpan(this.txtPNO, 2);
+            this.txtPNO.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtPNO.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtPNO.Location = new System.Drawing.Point(3, 3);
+            this.txtPNO.Name = "txtPNO";
+            this.txtPNO.Size = new System.Drawing.Size(794, 21);
+            this.txtPNO.TabIndex = 0;
             // 
-            // frmMain
+            // frmALC_Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -234,8 +229,9 @@ namespace ALC_SEJIN
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.statusStrip1);
-            this.Name = "frmMain";
+            this.Name = "frmALC_Main";
             this.Text = "ALC_SEJIN";
+            this.Load += new System.EventHandler(this.frmALC_Main_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.tableLayoutPanel1.ResumeLayout(false);
@@ -251,15 +247,14 @@ namespace ALC_SEJIN
         private System.Windows.Forms.ToolStripMenuItem tsmiZPLMenu;
         private System.Windows.Forms.ToolStripMenuItem tsmiPROGList;
         private System.Windows.Forms.ToolStripMenuItem tsmiURL;
-        private System.Windows.Forms.ToolStripMenuItem tsmiZPLData;
-        private System.Windows.Forms.ToolStripMenuItem tsmiZPLDataOption1;
-        private System.Windows.Forms.ToolStripMenuItem tsmiZPLDataOption2;
+        private System.Windows.Forms.ToolStripMenuItem tsmiZPLPrintSize;
+        private System.Windows.Forms.ToolStripMenuItem tsmiZPLPrintSizeOption1;
+        private System.Windows.Forms.ToolStripMenuItem tsmiZPLPrintSizeOption2;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private System.Windows.Forms.ToolStripMenuItem tsmiZPLSave;
-        private System.Windows.Forms.ToolStripMenuItem tsmiZPLExit;
         private System.Windows.Forms.ToolStripMenuItem tsmiExit;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtPNO;
         private System.Windows.Forms.TextBox txtTemp;
         private System.Windows.Forms.Button btnPrint;
         private System.Windows.Forms.Button btnShowJson;
--- ALC_SEJIN/frmALC_Main.cs
+++ ALC_SEJIN/frmALC_Main.cs
@@ -12,9 +12,35 @@ namespace ALC_SEJIN
 {
     public partial class frmALC_Main : Form
     {
+        private static string _progCD;
+        private static string _zplPrintSize;
+
+        private void DefultAppSetting()
+        {
+            _progCD = Properties.Settings.Default["zplPrintSize"].ToString();
+            _zplPrintSize = Properties.Settings.Default["zplPrintSize"].ToString();
+            foreach (ToolStripMenuItem item in tsmiZPLPrintSize.DropDownItems)
+            {
+                item.Checked = false;
+                if (_zplPrintSize.Equals(item.Text))
+                {
+                    item.Checked = true;
+                    this.tsmiZPLPrintSize.Text = "Print SIZE : " + item.Text;
+                    this.tsmiZPLPrintSize.Checked = true;
+                }
+            }
+        }
+
         public frmALC_Main()
         {
             InitializeComponent();
+        }
+        private void frmALC_Main_Load(object sender, EventArgs e)
+        {
+            this.ActiveControl = txtPNO;
+            this.txtPNO.Text = "87360AA000A5G";
+            this.txtPNO.SelectAll();
+            this.DefultAppSetting();
         }
 
         private void tsmiPROGListOption_Click(object sender, EventArgs e)
@@ -31,18 +57,58 @@ namespace ALC_SEJIN
             }
         }
 
-        private void tsmiZPLDataOption_Click(object sender, EventArgs e)
+        private void tsmiZPLPrintSizeOption_Click(object sender, EventArgs e)
         {
             //초기화
-            this.tsmiZPLDataOption1.Checked = false;
-            this.tsmiZPLDataOption2.Checked = false;
+            this.tsmiZPLPrintSizeOption1.Checked = false;
+            this.tsmiZPLPrintSizeOption2.Checked = false;
 
             ((ToolStripMenuItem)sender).Checked = true;
 
             if (((ToolStripMenuItem)sender).Checked)
             {
-                this.tsmiZPLData.Text = string.Concat("설정사이즈 : ", ((ToolStripMenuItem)sender).Text);
+                this.tsmiZPLPrintSize.Text = string.Concat("Print SIZE : ", ((ToolStripMenuItem)sender).Text);
+                _zplPrintSize = ((ToolStripMenuItem)sender).Text;
             }
         }
+
+        private void tsmiExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void tsmiZPLSave_Click(object sender, EventArgs e)
+        {
+            this.tsmiZPLPrintSize.DropDown.Close();
+            this.tsmiZPLPrintSize.DropDown.AutoClose = true;
+            this.tsmiZPLMenu.Enabled = true;
+
+            MessageBox.Show(_zplPrintSize);
+            Properties.Settings.Default["zplPrintSize"] = _zplPrintSize;
+            Properties.Settings.Default.Save();
+
+            //stutus 설정변경
+            string status = "Setting Saved";
+
+            //this.toolStripStatusLabel1.Text = status;
+            //log.Info(status);
+
+            ////세팅값 변경
+            //Properties.Settings.Default["zplDataType"] = _zplDataType;
+            //Properties.Settings.Default["optionList"] = this.tstbOptionListData.TextBox.Text;
+            //_optionList = Properties.Settings.Default["optionList"].ToString().Split(',');
+            //Properties.Settings.Default.Save();
+
+            ////stutus 설정변경
+            //string status = "Setting Saved";
+            //this.toolStripStatusLabel1.Text = status;
+            //log.Info(status);
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
